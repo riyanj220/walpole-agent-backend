089606d8e5079af0fb48edeb7c7d389e3ce0c1ed
Riyan Jamil <riyanjamil220@gmail.com>
2025-11-07 02:23:45 +0500

    fix: Update Django settings to include CSV import and set STATIC_ROOT; correct gunicorn command in docker-compose

--- backend/settings.py
+++ backend/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import csv
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,8 @@ SECRET_KEY = "django-insecure-r&5onp6!tuz82+a0kvn*1svcmwdepcuh1$x&)!p$ckw%7+=*#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=csv())
 
 
 # Application definition
@@ -117,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
